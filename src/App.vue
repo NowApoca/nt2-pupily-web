1cadad2b167587b40cd9f2c49f22a7a36d533a0d
morterox <martinjavieralper@gmail.com>
2021-11-08 01:50:26 -0300

    Home and start of login

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/register">Register</router-link>
-  </div>
+<nav class="navbar container" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/"><strong class="is-size-4">Pupily</strong></a>
+    </div>
+    <div id="navbar" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">Home</router-link>
+        <router-link to="/about" class="navbar-item">About</router-link>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-dark"><strong>Sign In</strong></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
   <router-view />
 </template>
 
@@ -16,17 +28,15 @@
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
+nav {
+	 margin-top: 25px;
+	 margin-bottom: 30px;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+ nav a {
+	 font-weight: bold;
+	 color: #2c3e50;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+ nav a.router-link-exact-active {
+	 color: #d88d00;
 }
 </style>
---
+++ src/components/PupilysList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="events container">
+    <h2 class="subtitle is-3">Check out our top achievers Pupilys</h2>
+    <div class="columns is-multiline">
+      <div class="column is-one-quarter">
+        <PupilyCard />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import PupilyCard from '@/components/PupilyCard';
+  export default {
+    name: 'PupilysList',
+    components: {
+      PupilyCard,
+    },
+  };
+</script>
+<style scoped>
+  .events {
+    margin-top: 100px;
+    text-align: center;
+  }
+</style>
